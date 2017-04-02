5395a2c30831c19dba6d2d43aee4e317bfed34b0
Simi Mutasa <stmutasa@gmail.com>
2017-04-02 18:36:12 -0400

    Transformer woes

--- src/BonaAge.py
+++ src/BonaAge.py
@@ -55,30 +55,39 @@ def forward_pass(images, phase_train=True, bts=0):
     # The 4th convolutional layer
     conv4 = convolution('Conv4', conv3, 3, 128, phase_train=phase_train)
 
-    # # To Do: Insert the affine transform layer here: Output of conv4 is [batch, 14,14,128]
+    # To Do: Insert the affine transform layer here: Output of conv4 is [batch, 14,14,128]
     # with tf.variable_scope('Transformer') as scope:
     #
-    #     # Set up the localisation network to calculate floc(u):
-    #     W1 = tf.get_variable('Weights1', shape=[FLAGS.batch_size, 14, 128, 20], initializer=tf.contrib.layers.xavier_initializer())
-    #     B1 = tf.get_variable('Bias1', shape=[20], initializer=tf.contrib.layers.xavier_initializer())
-    #     W2 = tf.get_variable('Weights2', shape=[FLAGS.batch_size, 14, 20, 6], initializer=tf.contrib.layers.xavier_initializer())
+    #     # Set up fully connected layer with 6 outputs
+    #     n_fc = 6
+    #     W_fc1 = tf.Variable(tf.zeros([14*14*128, n_fc]), name='W_fc1')
     #
     #     # Always start with the identity transformation
     #     initial = np.array([[1.0, 0, 0], [0, 1.0, 0]])
     #     initial = initial.astype('float32')
     #     initial = initial.flatten()
-    #     B2 = tf.Variable(initial_value=initial, name='IDFunction')
     #
-    #     # Define the two layers of the localisation network
-    #     H1 = tf.nn.tanh(tf.matmul(conv4, W1) + B1)
-    #     H2 = tf.nn.tanh(tf.matmul(H1, W2) + B2)
+    #     b_fc1 = tf.Variable(initial_value=initial, name='b_fc1')
+    #     h_fc1 = tf.matmul(tf.zeros([FLAGS.batch_size, 14*14*128]), W_fc1) + b_fc1
     #
-    #     # Define the output size to the original dimensions
-    #     output_size = (FLAGS.batch_size, 14, 14, 1)
-    #     trans = transformer([-1, 14, 14, 128], H2, output_size)
+    #     output_size = (14, 14)
+    #     h_trans = batch_transformer(conv4, h_fc1, output_size)
+    #
+    # # The 5th convolutional layer
+    # with tf.variable_scope('Conv5') as scope:
+    #     # Define the Kernel. Can use Xavier init: contrib.layers.xavier_initializer())
+    #     kernel = tf.get_variable('Weights', shape=[1, 1, 6, 128],
+    #                              initializer=tf.truncated_normal_initializer(stddev=5e-2))
+    #
+    #     # Perform the actual convolution
+    #     conv = tf.nn.conv2d(h_trans, kernel, [1, 2, 2, 1], padding='VALID')  # Create a 2D tensor with BATCH_SIZE rows
+    #
+    #     # Relu activation
+    #     conv5 = tf.nn.relu(conv, name=scope.name)
+    #
+    #     # Create a histogram/scalar summary of the conv1 layer
+    #     _activation_summary(conv)
 
-    # The 5th convolutional layer
-    # conv5 = convolution('Conv5', trans, 1, 3, 128, phase_train=phase_train)
     conv5 = convolution('Conv5', conv4, 3, 128, phase_train=phase_train)
 
     # The Fc7 layer
